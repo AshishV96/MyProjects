dd485c782743ca1b9c506f2d652b5892ad40cbaa
Ashish <ashishv@ca-one.com>
2024-07-14 11:46:21 +0530

    unchanged

--- Test/src/Codes.java
+++ Test/src/Codes.java
@@ -188,11 +188,4 @@ public class Codes {
         }
     }
 
-    public static void leftShift(int[] nums, int m) {
-
-        for (int i = nums.length - 1; i > m; i--) {
-            nums[i] = nums[i - 1];
-        }
-    }
-
 }
